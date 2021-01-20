f9192ce8890bc998324bfe7996fe466ce893e215
Sergiu Buhatel <sergiubuhatel@Sergius-MacBook-Pro-2.local>
2021-01-20 10:03:46 -0500

    added client secret

---
+++ src/client-configurations.js.prod.js
@@ -0,0 +1,34 @@
+class ClientConfig {
+  getServerURL() {
+    return 'https://permafrost.carleton.ca'
+  }
+  getAuthenticationURL () {
+    return this.getServerURL() + ':7022'
+  }
+  getApi() {
+    return this.getServerURL() + ':7022/flask_seed_api'
+  }
+
+  // Keycloak stuff
+  getRealm() {
+    return 'seed'
+  }
+  getClient() {
+    return 'seed'
+  }
+  getServer() {
+    return 'permafrost.carleton.ca'
+  }
+  getPort() {
+    return '7010'
+  }
+  getSecret() {
+    return '57d67ba2-c11a-40b6-be80-a74d0176b6d7'
+  }
+  isHTTPS() {
+    return true
+  }
+}
+
+export default ClientConfig
+
